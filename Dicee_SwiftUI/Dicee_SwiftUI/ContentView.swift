50b1b0bf2facf34748ae6f7c104f0b1bebc06442
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2020-08-02 20:43:00 +0700

    Init project

---
+++ Dicee_SwiftUI/Dicee_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee_SwiftUI
+//
+//  Created by Le Minh Khoi on 8/2/20.
+//  Copyright © 2020 Le Minh Khoi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
